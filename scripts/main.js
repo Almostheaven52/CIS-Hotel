339c64f87601af94bbcc45c986426ef9bee8f12d
Almostheaven52 <97544927+Almostheaven52@users.noreply.github.com>
2022-04-08 12:28:44 -0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+// I was struggling with making this work but i need to get this inso i dont get
+// a 0. Planning to do all the extra credit and stuff and add the functionality
+// When I understand what I'm doing.
